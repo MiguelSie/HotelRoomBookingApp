1f599ec8b47da5192e245a4810058c3fe9af8f87
MiguelSie <msarroyo@uninorte.edu.co>
2023-05-15 22:55:16 -0500

    Empezó la matazón

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,29 @@ from flask import render_template, flash, redirect, request, url_for
 from config import Config
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
+from forms import dateForm, bookingForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = '4b02b3db95d7a47c2a4b8a2ce3dfa116'
+
 app.config.from_object(Config)
 
-@app.route('/')
-@app.route('/book')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/home', methods = ['GET', 'POST'])
+def index():
+    form = dateForm()
+    
+    return render_template('index.html', title='Home', form=form)
+
+@app.route('/rooms', methods = ['GET', 'POST'])
+def rooms():
+    return render_template('rooms.html', title='Rooms')
+
+@app.route("/booking", methods = ['GET', 'POST'])
 def book():
-    return render_template('booking.html', title='Bookings')
+    form = bookingForm()
+    return render_template('booking.html', title='Bookings', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,29 @@
+from email.policy import default
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField, DateField, SelectMultipleField, widgets
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 import datetime
 
     
-class RoomChoiceIterable(object):
-    def __iter__(self):
-        rooms=Room.query.all()
-        yield from [(room.id,room.roomName) for room in rooms]
+#class RoomChoiceIterable(object):
+#    def __iter__(self):
+#        rooms=Room.query.all()
+#        yield from [(room.id,room.roomName) for room in rooms]
+
+class dateForm(FlaskForm):
+    dateStart=DateField('Fecha de inicio',validators=[DataRequired()],format='%d/%m/%Y')
+    dateFinish=DateField('Fecha de fin',validators=[DataRequired()],format='%d/%m/%Y')
+    totPeople=IntegerField('Cantidad de personas',validators=[DataRequired()], default=1)
+    submit=SubmitField('Reservar')
+    
+    def validate_date(self,date):
+        if self.date.data<datetime.datetime.now().date():
+            raise ValidationError('You can only book for day after today.')
         
-class BookmeetingForm(FlaskForm):
-    title=StringField('Meeting title',validators=[DataRequired()])
-    rooms=SelectField('Choose room',coerce=int,choices=RoomChoiceIterable())
-    date=DateField('Choose date', format="%m/%d/%Y",validators=[DataRequired()])
-    startTime=SelectField('Choose starting time(in 24hr expression)',coerce=int,choices=[(i,i) for i in range(9,19)])
-    duration=SelectField('Choose duration of the meeting(in hours)',coerce=int,choices=[(i,i) for i in range(1,6)])
-    submit=SubmitField('Book')
+class bookingForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    name = StringField('Nombre', validators=[DataRequired()])
+    restaurante = BooleanField('Restaurante', default=True)
+    parqueadero = BooleanField('Parqueadero', default=True)
+    transporte = BooleanField('transporte', default=True)
+    submit = SubmitField('Realizar reserva')
