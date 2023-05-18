9016aa2e15839f960115843754123ac1fd230fe0
MiguelSie <msarroyo@uninorte.edu.co>
2023-05-17 23:20:33 -0500

    anotaciones

--- app/__init__.py
+++ app/__init__.py
@@ -50,10 +50,16 @@ def index():
     if form.validate_on_submit():
         flash(f'Buscando vuelos para {form.dateStart.data} a {form.dateFinish.data} para {form.totPeople.data} personas', 'success')
         return redirect(url_for('book'))
+    #Necesito poder pasar la información de las fechas a room para que se compare con las fechas de reserva de las habitaciones (y con las fechas de paro de las habitaciones)
+    #O no sé si hacer la query de las habitaciones disponibles acá y se la paso a room para que las muestre o si hacerla allá enseguida.
     return render_template('index.html', title='Home', form=form)
 
 @app.route('/rooms', methods = ['GET', 'POST'])
 def rooms():
+    #Puedo hacer que encuentre las habitaciones disponibles en las fechas que se ingresen y, que a partir de ahí
+    #Escoja una de esas habitaciones y que esa sea la que se muestre en la página de habitaciones, pero sin el número,
+    #De forma que se muestre la información de la habitación, pero no el número de la habitación. 
+    #Esto se realiza una vez por tipo, y tener en cuenta la capacidad de la habitación. Esto también se manda a booking como parte de la info. de reserva.
     return render_template('rooms.html', title='Rooms')
 
 @app.route("/booking", methods = ['GET', 'POST'])
